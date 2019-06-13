ebd47d98c974849ac8f5dff31b25e4a857050927
Anton Nazarenko <anton@code-care.pro>
2019-06-13 16:30:34 +0300

    Added customized notidications

--- src/components/HFUI/HFUI.js
+++ src/components/HFUI/HFUI.js
@@ -1,8 +1,9 @@
 import React from 'react'
 // import { Spinner, Intent } from '@blueprintjs/core'
 import { Switch, Route } from 'react-router-dom'
+import { NotificationContainer } from 'react-notifications'
 // import _isEmpty from 'lodash/isEmpty'
-
+import Notification from '../Notification'
 // import APIComboDialog from '../APIComboDialog'
 import SideNavBar from '../../ui/SideNavBar'
 import StatusBar from '../../ui/StatusBar'
@@ -10,6 +11,7 @@ import BacktestingView from '../../pages/Backtesting'
 import SettingsView from '../../pages/Settings'
 import AlgoOrdersView from '../../pages/AlgoOrders'
 import DashboardView from '../../pages/Dashboard'
+
 
 export default class HFUI extends React.Component {
   constructor(props) {
@@ -67,6 +69,7 @@ export default class HFUI extends React.Component {
     return (
       <div className='hfui'>
         <div className='hfui_content__wrapper'>
+          <NotificationContainer />
           <Route component={SideNavBar} />
           <Route exact path='/' component={DashboardView} />
           <Route exact path='/index.html' component={DashboardView} /> 
