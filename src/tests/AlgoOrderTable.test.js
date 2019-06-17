b1b7b129bedbffbb54a9de183ba69a3c850860a9
Anton Nazarenko <anton@code-care.pro>
2019-06-17 12:59:29 +0300

    fixed tests for mac os, added first test

---
+++ src/tests/AlgoOrderTable.test.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import AlgoOrderTable from '../components/AlgoOrderTable'
+
+const algoOrders = [
+  [42, 'bfx-ping_pong', true, null, +(new Date(Date.now() - (4 * 60 * 60 * 1000)))],
+]
+
+const orders = [
+  // all we care about is gid & symbol for rendering ([_, gid, _, symbol])
+  [null, 0, null, 'tBTCUSD'],
+  [null, 0, null, 'tBTCUSD'],
+  [null, 0, null, 'tBTCUSD'],
+  [null, 0, null, 'tBTCUSD'],
+  // note strat (1) is stopped
+  [null, 2, null, 'tLEOUSD'],
+  [null, 2, null, 'tLEOUSD'],
+  [null, 42, null, 'tLEOUSD'],
+  [null, 42, null, 'tLEOUSD'],
+  [null, 42, null, 'tLEOUSD'],
+  [null, 42, null, 'tLEOUSD'],
+  [null, 42, null, 'tLEOUSD'],
+  [null, 42, null, 'tLEOUSD'],
+  [null, 42, null, 'tLEOUSD'],
+  [null, 42, null, 'tLEOUSD'],
+  [null, 42, null, 'tLEOUSD'],
+]
+
+test('Link changes the class when hovered', () => {
+  const component = renderer.create(
+    <AlgoOrderTable
+        algoOrders={algoOrders}
+        orders={orders}
+      />,
+  )
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
