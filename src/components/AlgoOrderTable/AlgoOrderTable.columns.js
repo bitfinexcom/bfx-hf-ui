11594129789689d78a6ac1fde706fac5f88df58d
Jacob Plaster <jacob.plaster@youview.com>
2019-07-12 15:49:32 +0700

    ui: color scheme changes and remove algo order table features

--- src/components/AlgoOrderTable/AlgoOrderTable.columns.js
+++ src/components/AlgoOrderTable/AlgoOrderTable.columns.js
@@ -4,39 +4,34 @@ import Switch from 'react-switch'
 
 
 export default [{
-  width: 200,
+  width: 300,
   label: 'Name',
   dataKey: 'name',
   cellRenderer: ({ rowData = {} }) => rowData.name
 }, {
-  width: 200,
+  width: 300,
   label: 'Created',
   dataKey: 'mts',
-  cellRenderer: ({ rowData = {} }) => (
-    new Date(rowData.mts).toLocaleString()
-  ),
+  cellRenderer: ({ rowData = {} }) => {
+    if (typeof rowData.mts === 'string') {
+      return rowData.mts
+    }
+    return new Date(rowData.mts).toLocaleString()
+  },
 }, {
-  width: 80,
-  label: 'Symbol',
-  dataKey: 'symbol',
-  cellRenderer: ({ rowData = {} }) => rowData.symbol
-}, {
-  width: 120,
-  label: 'Order Count',
-  dataKey: 'orderCount',
-  cellRenderer: ({ rowData = {} }) => rowData.orderCount
-}, {
-  width: 120,
+  width: 300,
   label: 'Status',
   dataKey: 'status',
   cellRenderer: ({ rowData = {} }) => rowData.status 
-}, {
-  width: 120,
-  label: 'Actions',
-  dataKey: 'gid',
-  cellRenderer: ({ rowData = {} }) => (
-    rowData.status === 'ACTIVE'
-      ? <Switch checked={true} height={14} width={28} onColor='#0F0' />
-      : <Switch checked={false} height={14} width={28} offColor='#C1C2C3' />
-  ),
-}]
+},
+// {
+//   width: 120,
+//   label: 'Actions',
+//   dataKey: 'gid',
+//   cellRenderer: ({ rowData = {} }) => (
+//     rowData.status === 'ACTIVE'
+//       ? <Switch checked={true} height={14} width={28} onColor='#0F0' />
+//       : <Switch checked={false} height={14} width={28} offColor='#C1C2C3' />
+//   ),
+// }
+]
--- src/pages/AlgoOrders/AlgoOrders.js
+++ src/pages/AlgoOrders/AlgoOrders.js
@@ -2,31 +2,11 @@ import React from 'react'
 import AlgoOrderTable from '../../components/AlgoOrderTable'
 
 const algoOrders = [
-  [42, 'bfx-ping_pong', true, null, +(new Date(Date.now() - (4 * 60 * 60 * 1000)))],
-  [42, 'bfx-iceberg', true, null, +(new Date(Date.now() - (4 * 60 * 60 * 1000)))],
-  [42, 'bfx-twap', true, null, +(new Date(Date.now() - (4 * 60 * 60 * 1000)))],
-  [42, 'bfx-accumulate_distribute', true, null, +(new Date(Date.now() - (4 * 60 * 60 * 1000)))],
+  [42, 'bfx-ping_pong', true, null, 'Default'],
+  [42, 'bfx-iceberg', true, null, 'Default'],
+  [42, 'bfx-twap', true, null, 'Default'],
+  [42, 'bfx-accumulate_distribute', true, null, 'Default'],
 ]
-const orders = [
-  // all we care about is gid & symbol for rendering ([_, gid, _, symbol])
-  [null, 0, null, 'tBTCUSD'],
-  [null, 0, null, 'tBTCUSD'],
-  [null, 0, null, 'tBTCUSD'],
-  [null, 0, null, 'tBTCUSD'],
-  // note strat (1) is stopped
-  [null, 2, null, 'tLEOUSD'],
-  [null, 2, null, 'tLEOUSD'],
-  [null, 42, null, 'tLEOUSD'],
-  [null, 42, null, 'tLEOUSD'],
-  [null, 42, null, 'tLEOUSD'],
-  [null, 42, null, 'tLEOUSD'],
-  [null, 42, null, 'tLEOUSD'],
-  [null, 42, null, 'tLEOUSD'],
-  [null, 42, null, 'tLEOUSD'],
-  [null, 42, null, 'tLEOUSD'],
-  [null, 42, null, 'tLEOUSD'],
-]
-
 
 export default class AlgoOrdersView extends React.Component {
   state = {}
@@ -37,7 +17,7 @@ export default class AlgoOrdersView extends React.Component {
         <h1>Algo Orders</h1>
         <AlgoOrderTable
           algoOrders={algoOrders}
-          orders={orders} />
+        />
       </div>
     )
   }
