c02f740b8adc9d6b174e68e872964408b52ba3a3
Antony <51406639+AntonNazarenko@users.noreply.github.com>
2019-08-07 21:07:59 +0300

    add menu

--- public/electron.js
+++ public/electron.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, protocol } = require('electron')
+const { app, BrowserWindow, protocol, Menu } = require('electron')
 const path = require('path')
 const url = require('url')
 
@@ -33,6 +33,29 @@ app.on('ready', () => {
   }, (err) => {
     if (err) console.error('Failed to register protocol')
   })
+  const template = [{
+    label: 'Application',
+    submenu: [
+      { label: 'About Application', selector: 'orderFrontStandardAboutPanel:' },
+      { type: 'separator' },
+      { label: 'Quit', accelerator: 'Command+Q', click() { app.quit() } },
+    ],
+  }, {
+    label: 'Edit',
+    submenu: [
+      { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+      { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+      { type: 'separator' },
+      { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+      { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+      { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+      { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' },
+    ],
+  },
+  ]
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+
   createWindow()
 })
 
