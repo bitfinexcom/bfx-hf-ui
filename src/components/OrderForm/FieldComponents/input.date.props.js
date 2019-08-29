08d3ab0ba400f40744ebdf4ec879916ab165b48b
Cris Mihalache <me@f3rno.com>
2019-08-29 14:47:33 +0700

    (fix) input.date.props

--- src/components/OrderForm/FieldComponents/input.date.props.js
+++ src/components/OrderForm/FieldComponents/input.date.props.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 export const propTypes = {
   def: PropTypes.object.isRequired,
   renderData: PropTypes.object.isRequired,
-  value: PropTypes.date.isRequired,
+  value: PropTypes.any.isRequired,
   onChange: PropTypes.func.isRequired,
   validationError: PropTypes.string,
 }
