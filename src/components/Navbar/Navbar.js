1885394eafa0fe2816717d97d6a3ddda79108174
Anton Nazarenko <anton@code-care.pro>
2019-09-30 20:27:07 +0300

    delete button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import axios from 'axios'
 
 import NavbarButton from '../NavbarButton'
 import { propTypes, defaultProps } from './Navbar.props'
@@ -11,18 +10,6 @@ export default class Navbar extends React.PureComponent {
   static defaultProps = defaultProps
 
   render() {
-    axios
-      .get('https://raw.githubusercontent.com/bitfinexcom/bfx-hf-ui/master/package.json')
-      .then(({ data }) => {
-        const lastVersion = data.version
-        // eslint-disable-next-line global-require
-        const currVersion = require('../../../package.json').version
-        console.log(lastVersion, currVersion, this.state)
-        // eslint-disable-next-line react/no-unused-state
-        this.setState({ lastVersion, currVersion })
-      })
-    const { lastVersion, currVersion } = this.state || {}
-
     return (
       <div className='hfui-navbar__wrapper'>
         <ul className='hfui-navbar__main-links'>
@@ -46,16 +33,6 @@ export default class Navbar extends React.PureComponent {
               label='Strategy Editor'
             />
           </li>
-          {
-              lastVersion && lastVersion !== currVersion ? (
-                <li style={{ marginTop: '9px' }}>
-                  <NavbarButton
-                    label='Update to last version'
-                    external='https://github.com/bitfinexcom/bfx-hf-ui/releases'
-                  />
-                </li>
-              ) : null
-          }
         </ul>
       </div>
     )
