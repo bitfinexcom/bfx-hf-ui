e668cb7893354f062dfdb9f449e55d2a49747e83
Cris Mihalache <me@f3rno.com>
2019-10-11 18:14:05 +0700

    (feature) mock up order form menu

---
+++ src/components/OrderForm/OrderFormMenu/OrderFormMenu.props.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types'
+
+export const propTypes = {
+  atomicOrderTypes: PropTypes.array.isRequired,
+  algoOrderTypes: PropTypes.array.isRequired,
+  onSelect: PropTypes.func.isRequired,
+}
+
+export const defaultProps = {
+
+}
