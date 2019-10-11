9920321400e2885356b53af2c5aff56c563fba65
Cris Mihalache <me@f3rno.com>
2019-10-11 20:33:54 +0700

    (feature) convert remote version check into loop on interval

--- src/redux/sagas/ui/worker_fetch_remote_version.js
+++ src/redux/sagas/ui/worker_fetch_remote_version.js
@@ -1,24 +1,28 @@
-import { put } from 'redux-saga/effects'
+import { put, delay } from 'redux-saga/effects'
 import Debug from 'debug'
 import Request from 'request-promise'
 
 import UIActions from '../../actions/ui'
 
+const CHECK_INTERVAL_MS = 60 * 60 * 1000 // 1hr
 const REMOTE_MANIFEST_URL = 'https://raw.githubusercontent.com/bitfinexcom/bfx-hf-ui/master/package.json'
 
 const debug = Debug('hfui:rx:s:ws-hfui:worker-fetch-remote-version')
 
 export default function* () {
-  let remoteManifestData
+  while (true) {
+    let remoteManifestData
 
-  try {
-    remoteManifestData = yield Request(REMOTE_MANIFEST_URL)
-  } catch (err) {
-    debug('failed to fetch remote manifest: %s', err.message)
-    return
+    try {
+      remoteManifestData = yield Request(REMOTE_MANIFEST_URL)
+    } catch (err) {
+      debug('failed to fetch remote manifest: %s', err.message)
+      return
+    }
+
+    const { version } = remoteManifestData
+
+    yield put(UIActions.saveRemoteVersion(version))
+    yield delay(CHECK_INTERVAL_MS)
   }
-
-  const { version } = remoteManifestData
-
-  yield put(UIActions.saveRemoteVersion(version))
 }
