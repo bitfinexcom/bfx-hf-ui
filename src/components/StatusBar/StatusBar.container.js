9ec516f4b976f72bd8277fb91bc357eef2180ffd
Anton Nazarenko <anton@code-care.pro>
2019-11-05 12:58:18 +0200

    add currentExchange

--- src/components/StatusBar/StatusBar.container.js
+++ src/components/StatusBar/StatusBar.container.js
@@ -3,16 +3,21 @@ import { push } from 'connected-react-router'
 
 import StatusBar from './StatusBar'
 import { getNumberOfLayouts, getRemoteVersion } from '../../redux/selectors/ui'
-import { getSocket } from '../../redux/selectors/ws'
+import { getSocket, getAPIClientStates } from '../../redux/selectors/ws'
+
 
 const mapStateToProps = (state = {}) => {
   const socket = getSocket(state)
   const { status: wsStatus } = socket
+  const { ui } = state
+  const exchange = ui.activeExchange
 
   return {
     wsConnected: wsStatus === 'online',
     nLayouts: getNumberOfLayouts(state),
     remoteVersion: getRemoteVersion(state),
+    apiClientStates: getAPIClientStates(state),
+    currentExchange: exchange,
   }
 }
 
