7d87856729588064d188417dd6a159ff3cb5cbeb
Anton Nazarenko <anton@code-care.pro>
2019-12-05 20:19:29 +0200

    add settings route

--- src/components/HFUI/HFUI.js
+++ src/components/HFUI/HFUI.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Switch, Redirect } from 'react-router'
 
+import SettingsPage from '../../pages/Settings'
 import TradingPage from '../../pages/Trading'
 import StrategyEditorPage from '../../pages/StrategyEditor'
 import MarketDataPage from '../../pages/MarketData'
@@ -76,6 +77,14 @@ export default class HFUI extends React.PureComponent {
               <MarketDataPage />
             )}
           />
+
+          <Route
+            path='/settings'
+            render={() => (
+              <SettingsPage />
+            )}
+          />
+
         </Switch>
 
         <NotificationsSidebar />
