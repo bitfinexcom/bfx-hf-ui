50473ae1a4261981d07449bd6d3b32e476225d94
Cris Mihalache <me@f3rno.com>
2020-01-30 16:39:19 +0700

    (refactor) tweak/lint settings page for release

---
+++ src/pages/Settings/Settings.props.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+
+export const propTypes = {
+  authToken: PropTypes.string,
+  savedState: PropTypes.object,
+  activeExchange: PropTypes.string,
+  chart: PropTypes.string,
+  theme: PropTypes.string,
+  dms: PropTypes.number,
+}
+
+export const defaultProps = {}
