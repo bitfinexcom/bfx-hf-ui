6fb402d3c9c311eb09a2a74c4e535e947f913b63
Cris Mihalache <me@f3rno.com>
2020-03-06 16:30:41 +0700

    (fix) get jest running

---
+++ jest/cssTransform.js
@@ -0,0 +1,15 @@
+// This file was initially borrowed from the result of running `react-scripts eject`
+
+
+// This is a custom Jest transformer turning style imports into empty objects.
+// http://facebook.github.io/jest/docs/en/webpack.html
+
+module.exports = {
+  process() {
+    return 'module.exports = {};'
+  },
+  getCacheKey() {
+    // The output is always the same.
+    return 'cssTransform'
+  },
+}
