14c594aff9b54c578ca470c77348385cc589701f
Anton Nazarenko <anton@code-care.pro>
2020-04-01 20:38:31 +0300

    settings test

---
+++ src/pages/Settings/Settings.test.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { configure, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { Provider } from "react-redux"
+import configureMockStore from "redux-mock-store"
+import Settings from './Settings'
+
+
+const mockStore = configureMockStore()
+const store = mockStore({
+    ui: {
+        activeExchange: 'bitfinex',
+    }, 
+    settings: {
+        dms: true
+    }
+})
+
+configure({ adapter: new Adapter() })
+
+describe('Settings', () => {
+    test('should render settings page', () => {
+        const component = mount((
+            <Provider store={store} >
+                <Settings />
+            </Provider>
+        ))
+
+        expect(component).toMatchSnapshot()
+    })
+
+    test('find two inputs for api key & api secret', () => {
+        const component = mount((
+            <Provider store={store} >
+                <Settings />
+            </Provider>
+        ))
+        expect(component.find('.hfui-settings__option').length).toBe(2)
+    })
+})
