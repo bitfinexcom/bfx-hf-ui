d46caa904f47c32a514f87ff1d47fffeae81f04e
Anton Nazarenko <anton@code-care.pro>
2020-04-07 23:13:58 +0300

    update algo orders table test

--- src/components/AlgoOrdersTable/AlgoOrdersTable.test.js
+++ src/components/AlgoOrdersTable/AlgoOrdersTable.test.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { configure, shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-
+import data from './ao.json'
 import AlgoOrdersTable from './AlgoOrdersTable'
+import Test from '../../testEntities'
+
+
 
 configure({ adapter: new Adapter() })
 
 describe('AlgoOrdersTable', () => {
-    const data = [{"gid":"1585330269533","name":"MA Crossover","label":"MA Crossover | 1 @ MARKET  | long EMA(1) | short EMA(0)","args":{"shortType":"EMA","longType":"EMA","amount":1,"shortMAPeriod":0,"longMAPeriod":0,"shortEMAPeriod":0,"longEMAPeriod":1,"shortMAPrice":"CLOSE","shortEMAPrice":"CLOSE","longMAPrice":"CLOSE","longEMAPrice":"CLOSE","shortMATF":"1m","longMATF":"1m","shortEMATF":"1m","longEMATF":"1m","orderType":"MARKET","orderPrice":0,"submitDelay":2,"cancelDelay":1,"_margin":false,"_futures":false,"symbol":"tBTCUSD","long":{"type":"ema","candlePrice":"close","candleTimeFrame":"1m","args":[1]},"short":{"type":"ema","candlePrice":"close","candleTimeFrame":"1m","args":[0]}},"exID":"bitfinex"}]
     test('it renders the algo orders table', () => {
         const component = mount((
             <AlgoOrdersTable
@@ -32,7 +34,7 @@ describe('AlgoOrdersTable', () => {
                 apiClientState={2}
             />
         ))
-        component.find('.fa.fa-stop').simulate('click')
 
+        Test.fireEvent(component.find('.fa.fa-stop'), 'click')
     })
 })
