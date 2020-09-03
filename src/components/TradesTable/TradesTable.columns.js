504f4599f5fed06e8e4d62c300badba782378fad
Anton Nazarenko <anton@code-care.pro>
2020-09-03 19:49:34 +0300

    trades table i18n

--- src/components/TradesTable/TradesTable.columns.js
+++ src/components/TradesTable/TradesTable.columns.js
@@ -3,8 +3,13 @@ import BigN from 'bignumber.js'
 import { prepareAmount } from 'bfx-api-node-util'
 import PLNumber from '../../ui/PLNumber'
 
+import i18n from './i18n.json'
+
+console.log(i18n)
+const dictionary = i18n['ru-RU']
+
 export default [{
-  label: 'Time',
+  label: dictionary.time,
   dataKey: 'mts',
   width: 100,
   cellRenderer: ({ rowData = {} }) => ( // eslint-disable-line
@@ -14,7 +19,7 @@ export default [{
     />
   ),
 }, {
-  label: 'Price',
+  label: dictionary.price,
   dataKey: 'price',
   width: 120,
   cellRenderer: ({ rowData = {} }) => ( // eslint-disable-line
@@ -24,7 +29,7 @@ export default [{
     />
   ),
 }, {
-  label: 'Total',
+  label: dictionary.amount,
   dataKey: 'amount',
   width: 120,
   cellRenderer: ({ rowData = {} }) => ( // eslint-disable-line
