60cf15f4368527bb8221881b9c844b06d2bff7b8
Anton Nazarenko <anton@code-care.pro>
2020-09-22 22:44:05 +0300

    balances table i18n

--- src/components/BalancesTable/BalancesTable.columns.js
+++ src/components/BalancesTable/BalancesTable.columns.js
@@ -1,28 +1,28 @@
 import _capitalize from 'lodash/capitalize'
 import { prepareAmount } from 'bfx-api-node-util'
 
-export default () => [{
-  label: 'Exchange',
+export default (dictionary) => [{
+  label: dictionary.exchange,
   dataKey: 'exID',
   width: 120,
   cellRenderer: ({ rowData = {} }) => _capitalize(rowData.exID),
 }, {
-  label: 'Context',
+  label: dictionary.context,
   dataKey: 'context',
   width: 120,
   cellRenderer: ({ rowData = {} }) => _capitalize(rowData.context),
 }, {
-  label: 'Currency',
+  label: dictionary.currency,
   dataKey: 'currency',
   width: 100,
   cellRenderer: ({ rowData = {} }) => rowData.currency,
 }, {
-  label: 'Balance',
+  label: dictionary.balance,
   dataKey: 'balance',
   width: 120,
   cellRenderer: ({ rowData = {} }) => prepareAmount(rowData.balance),
 }, {
-  label: 'Available',
+  label: dictionary.available,
   dataKey: 'available',
   width: 120,
   cellRenderer: ({ rowData = {} }) => prepareAmount(rowData.available),
--- src/components/BalancesTable/BalancesTable.js
+++ src/components/BalancesTable/BalancesTable.js
@@ -6,13 +6,17 @@ import Table from '../../ui/Table'
 
 import './style.css'
 
+import i18n from './i18n.json'
+
 export default class BalancesTable extends React.PureComponent {
   static propTypes = propTypes
   static defaultProps = defaultProps
 
   render() {
-    const { balances = [], hideZeroBalances } = this.props
-
+    const {
+      balances = [], hideZeroBalances, lang,
+    } = this.props
+    const dictionary = i18n[lang]
     const filteredBalances = hideZeroBalances
       ? balances.filter(b => +b.balance > 0)
       : balances
@@ -20,7 +24,7 @@ export default class BalancesTable extends React.PureComponent {
     return (
       <Table
         data={filteredBalances}
-        columns={BalancesTableColumns()}
+        columns={BalancesTableColumns(dictionary)}
         defaultSortBy='mts'
         defaultSortDirection='ASC'
       />
