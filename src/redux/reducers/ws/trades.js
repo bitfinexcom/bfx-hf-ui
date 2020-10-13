5c6011ceea38ff0a4893bd831b712b0ec400ea44
Robert Kowalski <rok@kowalski.gd>
2020-10-13 15:36:07 +0200

    cleanup: remove dead code

--- src/redux/reducers/ws/trades.js
+++ src/redux/reducers/ws/trades.js
@@ -9,24 +9,6 @@ export default function (state = getInitialState(), action = {}) {
   const { type, payload = {} } = action
 
   switch (type) {
-    case t.DATA_TRADE: {
-      const { exID, channel, trade = {} } = payload
-      const [, market] = channel
-      const symbol = market.uiID
-
-      return {
-        ...state,
-
-        [exID]: {
-          ...(state[exID] || {}),
-          [symbol]: [
-            trade,
-            ...((state[exID] || {})[symbol] || []).slice(0, MAX_STORED_TRADES - 1),
-          ],
-        },
-      }
-    }
-
     case t.DATA_TRADES: {
       const { exID, channel, trades = [] } = payload
       const [, market] = channel
