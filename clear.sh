ae6f6bc8bdb6f449074484fcd28cdb643edac064
Robert Kowalski <rok@kowalski.gd>
2020-10-19 22:03:42 +0200

    build: don't delete bfx-hf-backtest
    the regex matches one of our modules, `bfx-hf-backtest`, which was
    then missing after building on the ci.

--- clear.sh
+++ clear.sh
@@ -1,4 +1,3 @@
 find ./node_modules -type d -name '*babel*' -maxdepth 1 -exec rm -rf {} \;
 find ./node_modules -type d -name '*css*'   -maxdepth 1 -exec rm -rf {} \;
 find ./node_modules -type d -name '*jest*'  -maxdepth 1 -exec rm -rf {} \;
-find ./node_modules -type d -name '*test*'  -maxdepth 1 -exec rm -rf {} \;
