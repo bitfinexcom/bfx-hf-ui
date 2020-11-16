d70128c7c3a706ee53c988604aa9f9d8b0a98508
Anton Nazarenko <anton@code-care.pro>
2020-11-16 18:35:22 +0200

    improve order history table

---
+++ src/components/OrderHistoryTable/OrderHistoryTable.helpers.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+const symbolToLabel = (symbol) => {
+  return `${symbol.substring(1, 4)}/${symbol.substring(4)}`
+}
+const getPriceFromStatus = (status) => {
+    if(status.includes('CANCELED')) {
+        return '0.00'
+    }
+    return `${status.split('@')[1].split('(')[0]}`
+}
+const getFormatedStatus = (status) => {
+    return `${status.split('@')[0]}`
+} 
+export {
+    symbolToLabel,
+    getPriceFromStatus,
+    getFormatedStatus,
+} 
+ 
