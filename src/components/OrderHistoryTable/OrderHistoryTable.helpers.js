fd083635bb1485fe6e7adbd211a22dff4bc97ed7
Anton Nazarenko <anton@code-care.pro>
2020-11-16 19:08:21 +0200

    eslint fix

--- src/components/OrderHistoryTable/OrderHistoryTable.helpers.js
+++ src/components/OrderHistoryTable/OrderHistoryTable.helpers.js
@@ -1,19 +1,17 @@
-/* eslint-disable */
 const symbolToLabel = (symbol) => {
   return `${symbol.substring(1, 4)}/${symbol.substring(4)}`
 }
 const getPriceFromStatus = (status) => {
-    if(status.includes('CANCELED')) {
-        return '0.00'
-    }
-    return `${status.split('@')[1].split('(')[0]}`
+  if (status.includes('CANCELED')) {
+    return '0.00'
+  }
+  return `${status.split('@')[1].split('(')[0]}`
 }
 const getFormatedStatus = (status) => {
-    return `${status.split('@')[0]}`
-} 
+  return `${status.split('@')[0]}`
+}
 export {
-    symbolToLabel,
-    getPriceFromStatus,
-    getFormatedStatus,
-} 
- 
+  symbolToLabel,
+  getPriceFromStatus,
+  getFormatedStatus,
+} //eslint-disable-line 
