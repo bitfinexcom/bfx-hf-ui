5ec7507d1a0dcfcbc8a809475605f208b3bc68ad
Cris Mihalache <xf3rno@pm.me>
2021-02-01 18:02:23 +0700

    fix: logs dir path in menu def and add API/DS server log file entries

---
+++ public/constants.js
@@ -0,0 +1,21 @@
+const os = require('os')
+
+const LOG_PATH = `${os.tmpdir()}/bfx-hf-ui-logs`
+const LOG_PATH_DS_BITFINEX = `${LOG_PATH}/ds-bitfinex-server.log`
+const LOG_PATH_API_SERVER = `${LOG_PATH}/api-server.log`
+
+const SCRIPT_PATH = `${__dirname}/../scripts`
+const SCRIPT_PATH_DS_BITFINEX = `${SCRIPT_PATH}/start-ds-bitfinex.js`
+const SCRIPT_PATH_API_SERVER = `${SCRIPT_PATH}/start-api-server.js`
+
+const LOCAL_STORE_CWD = `${os.homedir()}/.honeyframework`
+
+module.exports = {
+  LOG_PATH,
+  LOG_PATH_DS_BITFINEX,
+  LOG_PATH_API_SERVER,
+  SCRIPT_PATH,
+  SCRIPT_PATH_DS_BITFINEX,
+  SCRIPT_PATH_API_SERVER,
+  LOCAL_STORE_CWD
+}
