39cb535467af9143a56b54b4d916c75b30584ebd
Abhishek Shrestha <avsek.stha07@gmail.com>
2021-03-08 15:18:13 +0545

    check for negative values and greater than 100

--- src/components/OrderForm/FieldComponents/input.percent.js
+++ src/components/OrderForm/FieldComponents/input.percent.js
@@ -3,6 +3,20 @@ import NumberInput from './input.number'
 export default class PercentInput extends NumberInput {
   static processValue = v => (v / 100.0)
   static validateValue = (v) => {
-    return NumberInput.validateValue(v)
+    const numericError = NumberInput.validateValue(v)
+
+    if (numericError) {
+      return numericError
+    }
+
+    if (+v < 0) {
+      return 'Must be greater than 0'
+    }
+
+    if (+v > 100) {
+      return 'Must be less than 100'
+    }
+
+    return null
   }
 }
