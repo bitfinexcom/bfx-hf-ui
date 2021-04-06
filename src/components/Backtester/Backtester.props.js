c130c886cc7db84bda7be2bbf1da298c0c674cb1
Blaumaus <durexp@protonmail.ch>
2021-04-06 18:42:38 +0300

    Refactored component props

--- src/components/Backtester/Backtester.props.js
+++ src/components/Backtester/Backtester.props.js
@@ -2,8 +2,40 @@ import PropTypes from 'prop-types'
 
 export const propTypes = {
   indicators: PropTypes.array,
+  backtest: PropTypes.shape({
+    loading: PropTypes.bool.isRequired,
+    executing: PropTypes.bool.isRequired,
+  }),
+  backtestData: PropTypes.shape({
+    trades: PropTypes.array.isRequired,
+    candles: PropTypes.array.isRequired,
+  }),
+  strategyContent: PropTypes.objectOf(
+    PropTypes.oneOfType([
+      PropTypes.string.isRequired,
+      PropTypes.oneOf([null]).isRequired,
+    ]),
+  ),
+  allMarkets: PropTypes.object,
+  backtestResults: PropTypes.object,
+  backtestOptions: PropTypes.object,
+  authToken: PropTypes.string.isRequired,
+  dsExecuteBacktest: PropTypes.func.isRequired,
+  setBacktestOptions: PropTypes.func.isRequired,
 }
 
 export const defaultProps = {
   indicators: [],
+  backtest: {
+    loading: true,
+    executing: false,
+  },
+  backtestData: {
+    trades: [],
+    candles: [],
+  },
+  strategyContent: {},
+  allMarkets: {},
+  backtestResults: {},
+  backtestOptions: {},
 }
