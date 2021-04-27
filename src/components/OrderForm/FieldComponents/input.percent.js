5d3cce6773600d2eb39e921ef7baa416aefe7da4
Blaumaus <durexp@protonmail.ch>
2021-04-27 15:52:41 +0300

    Implemented limitations for percent inputs

--- src/components/OrderForm/FieldComponents/input.percent.js
+++ src/components/OrderForm/FieldComponents/input.percent.js
@@ -1,8 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
 import NumberInput from './input.number'
 
-export default class PercentInput extends NumberInput {
-  static processValue = v => (v / 100.0)
-  static validateValue = (v) => {
-    return NumberInput.validateValue(v)
-  }
+const PercentInput = ({ layout, ...props }) => {
+  const { id } = layout
+
+  return (
+    <NumberInput
+      {...props}
+      layout={layout}
+      max={id === 'bfx-iceberg' ? 100 : undefined}
+      percentage
+    />
+  )
 }
+
+PercentInput.propTypes = {
+  layout: PropTypes.objectOf(PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.array,
+    PropTypes.object,
+  ])).isRequired,
+}
+
+export default React.memo(PercentInput)
