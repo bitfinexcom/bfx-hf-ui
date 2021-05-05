42ef4bfa04aa3374f26f4340586428021aefa4a6
Jason Hasperhoven <martijn.hasperhoven@gmail.com>
2021-05-05 17:56:34 +0400

    Fix orderform dropdown & input bg colors

--- src/components/ExchangeInfoBar/ExchangeInfoBar.Button.js
+++ src/components/ExchangeInfoBar/ExchangeInfoBar.Button.js
@@ -1,23 +1,5 @@
 import React from 'react'
 
-// {(showSave) && (
-//   <div className='hfui-exchangeinfobar__right' onClick={onSave}>
-//     <i className='icon-save' />
-//   </div>
-// )}
-
-// {(showAddComponent) && (
-//   <div className='hfui-exchangeinfobar__right' onClick={onAddComponent}>
-//     <i className='icon-plus' />
-//   </div>
-// )}
-
-// {(showNotifications) && (
-//   <div className='hfui-exchangeinfobar__right' onClick={openNotifications}>
-//     <i className='icon-notifications' />
-//   </div>
-// )}
-
 export default function ExchangeInfoBarButton({ icon, ...props }) {
   return (
     <button type='button' className='hfui-exchangeinfobar__button' {...props}>
