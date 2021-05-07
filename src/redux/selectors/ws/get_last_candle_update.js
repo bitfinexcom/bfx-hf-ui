a93e9b7fd6a26f288b20e9f45ee40cb3724c5999
alexstotsky <alexander.stotsky@code-care.pro>
2021-05-07 16:43:55 +0300

    Clean outdated selectors

--- src/redux/selectors/ws/get_last_candle_update.js
+++
@@ -1,8 +0,0 @@
-import _get from 'lodash/get'
-import { REDUCER_PATHS } from '../../config'
-
-const path = REDUCER_PATHS.WS
-
-export default (state, { symbol, tf }) => {
-  return _get(state, `${path}.candles.lastUpdate`, {})[`${symbol}:${tf}`] || 0
-}
--- src/redux/selectors/ws/get_order_book.js
+++
@@ -1,8 +0,0 @@
-import _get from 'lodash/get'
-import { REDUCER_PATHS } from '../../config'
-
-const path = REDUCER_PATHS.WS
-
-export default (state, market) => {
-  return _get(state, `${path}.books.${market.uiID}`, [])
-}
