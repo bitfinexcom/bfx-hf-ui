40d550093ed9a994a517143ec50fec0895bbb88c
alexstotsky <alexander.stotsky@code-care.pro>
2021-05-11 15:30:40 +0300

    Add ExchngeInfoBarButton prop-types

--- src/components/ExchangeInfoBar/ExchangeInfoBar.Button.js
+++ src/components/ExchangeInfoBar/ExchangeInfoBar.Button.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default function ExchangeInfoBarButton({ icon, ...props }) {
   return (
@@ -6,4 +7,8 @@ export default function ExchangeInfoBarButton({ icon, ...props }) {
       <i className={`icon-${icon}`} />
     </button>
   )
+}
+
+ExchangeInfoBarButton.propTypes = {
+  icon: PropTypes.string.isRequired,
 }
