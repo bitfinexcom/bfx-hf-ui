aac6e488adbb9f0125187649ace4208dc242480b
Andrii R <durexp@protonmail.ch>
2021-05-25 07:52:28 +0000

    Integrated ufx-ui VirtualTable (#393)

--- src/components/AlgoOrdersTable/AlgoOrdersTable.container.js
+++ src/components/AlgoOrdersTable/AlgoOrdersTable.container.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux'
 import Debug from 'debug'
 
-import { getAlgoOrders, getAPIClientState, getAuthToken } from '../../redux/selectors/ws'
+import { getAlgoOrders, getAuthToken } from '../../redux/selectors/ws'
 import { getActiveMarket } from '../../redux/selectors/ui'
 import WSActions from '../../redux/actions/ws'
 import GAActions from '../../redux/actions/google_analytics'
@@ -12,8 +12,8 @@ const debug = Debug('hfui:c:algo-orders-table')
 const mapStateToProps = (state = {}) => {
   const { filteredAO = [] } = state.ui
   const activeMarket = getActiveMarket(state)
+
   return {
-    apiClientState: getAPIClientState(state),
     authToken: getAuthToken(state),
     algoOrders: getAlgoOrders(state),
     activeMarket,
@@ -25,8 +25,8 @@ const mapDispatchToProps = dispatch => ({
   cancelOrder: (authToken, order) => {
     const { gid } = order
 
-    debug('cancelling algo order %d', +gid)
-    dispatch(WSActions.send(['algo_order.cancel', authToken, 'bitfinex', `${gid}`]))
+    debug('cancelling algo order %d', gid)
+    dispatch(WSActions.send(['algo_order.cancel', authToken, 'bitfinex', gid]))
   },
   gaCancelOrder: () => {
     dispatch(GAActions.cancelAO())
