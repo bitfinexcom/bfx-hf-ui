1f5b46e49c2134990ecf28be545b779f6b0815b6
Blaumaus <durexp@protonmail.ch>
2021-05-26 16:30:22 +0300

    Refactored Orderform Modals

--- src/components/OrderForm/Modals/SubmitAPIKeysModal/SubmitAPIKeysModal.props.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types'
-
-export const propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-  onClose: PropTypes.func.isRequired,
-  isPaperTrading: PropTypes.bool.isRequired,
-}
-
-export const defaultProps = {}
