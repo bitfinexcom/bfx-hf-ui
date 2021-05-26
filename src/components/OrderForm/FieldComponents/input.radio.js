310c4696cd6cbef0159efe1f0ba41b9320857e54
Andrii R <durexp@protonmail.ch>
2021-05-26 10:48:11 +0000

    fix: radio buttons (#416)
    * Installed pretty-checkbox
    * Refactored 'input.radio' and 'ui.checkboxGroup'
    * Fixed Checkbox alignment

--- src/components/OrderForm/FieldComponents/input.radio.js
+++ src/components/OrderForm/FieldComponents/input.radio.js
@@ -1,34 +1,44 @@
-import React from 'react'
+import React, { memo } from 'react'
 import ClassNames from 'classnames'
+import PropTypes from 'prop-types'
+import _map from 'lodash/map'
 
 import RadioButton from '../../../ui/RadioButton'
 import { renderString } from '../OrderForm.helpers'
-import { propTypes, defaultProps } from './input.radio.props'
 
-export default class RadioInput extends React.PureComponent {
-  static propTypes = propTypes
-  static defaultProps = defaultProps
+const RadioInput = memo(({
+  def: { options }, renderData, value, onChange, disabled,
+}) => (
+  <div className={ClassNames('hfui-orderform__input', { disabled })}>
+    {_map(options, (o, i) => (
+      <RadioButton
+        key={i} //eslint-disable-line
+        label={renderString(o, renderData)}
+        value={value === o}
+        onChange={() => onChange(o)}
+        uppercase
+      />
+    ))}
+  </div>
+))
 
-  render() {
-    const {
-      def = {}, renderData, value, onChange, disabled,
-    } = this.props
-    const { options } = def
-    return (
-      <div className={ClassNames('hfui-orderform__input', {
-        disabled,
-      })}
-      >
-        {options.map((o, i) => (
-          <RadioButton
-            key={i} //eslint-disable-line
-            label={renderString(o, renderData)}
-            value={value === o}
-            onChange={() => onChange(o)}
-            uppercase
-          />
-        ))}
-      </div>
-    )
-  }
+RadioInput.displayName = 'RadioInput'
+
+RadioInput.propTypes = {
+  def: PropTypes.objectOf(PropTypes.oneOfType([
+    PropTypes.string, PropTypes.bool, PropTypes.array,
+  ])).isRequired,
+  renderData: PropTypes.shape({
+    QUOTE: PropTypes.string.isRequired,
+    BASE: PropTypes.string.isRequired,
+  }).isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
 }
+
+RadioInput.defaultProps = {
+  disabled: false,
+}
+
+export default RadioInput
