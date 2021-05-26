df2f51fe89ce1232936ac7adceaa611f088bb1fb
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-05-26 17:59:51 +0300

    Authentication: switched components to functional components && fixed prop types

--- src/pages/Authentication/AuthenticationConnectingForm.js
+++ src/pages/Authentication/AuthenticationConnectingForm.js
@@ -1,14 +1,12 @@
 import React from 'react'
 
-export default class AuthenticationInit extends React.Component {
-  render() {
-    return (
-      <div className='hfui-authenticationpage__content'>
-        <h2>Honey Framework UI</h2>
-        <p>Starting up app...</p>
+const AuthenticationConnectingForm = () => (
+  <div className='hfui-authenticationpage__content'>
+    <h2>Honey Framework UI</h2>
+    <p>Starting up app...</p>
 
-        <form className='hfui-authenticationpage__inner-form' />
-      </div>
-    )
-  }
-}
+    <form className='hfui-authenticationpage__inner-form' />
+  </div>
+)
+
+export default AuthenticationConnectingForm
