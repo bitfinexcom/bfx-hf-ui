8a1825e93f3d454c78c0f90a782b7be35e29604c
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-05-26 20:04:32 +0300

    MarketData: removed unused selectors and dispatch methods

--- src/pages/MarketData/MarketData.container.js
+++ src/pages/MarketData/MarketData.container.js
@@ -9,25 +9,11 @@ import UIActions from '../../redux/actions/ui'
 import MarketData from './MarketData'
 
 const mapStateToProps = (state = {}) => ({
-  layouts: getLayouts(state),
-  activeMarket: getActiveMarket(state),
   isFirstLogin: getFirstLogin(state),
   isGuideActive: getGuideStatusForPage(state, MARKET_PAGE),
 })
 
 const mapDispatchToProps = dispatch => ({
-  saveLayout: (layout, id) => {
-    dispatch(UIActions.saveLayout(layout, id))
-  },
-
-  createLayout: (id) => {
-    dispatch(UIActions.createLayout(id))
-  },
-
-  deleteLayout: (id) => {
-    dispatch(UIActions.deleteLayout(id))
-  },
-
   finishGuide() {
     dispatch(UIActions.finishGuide(MARKET_PAGE))
   },
