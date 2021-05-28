6b4e528545e095c9f5df6b2d4e932de57359f8da
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-05-28 18:18:33 +0300

    AtomicOrdersTablePanel: switched to fn components && removed unused code

--- src/components/AtomicOrdersTablePanel/AtomicOrdersTablePanel.container.js
+++ src/components/AtomicOrdersTablePanel/AtomicOrdersTablePanel.container.js
@@ -1,29 +1,19 @@
 import { connect } from 'react-redux'
 
-import { getComponentState, getActiveMarket } from '../../redux/selectors/ui'
+import { getActiveMarket } from '../../redux/selectors/ui'
 import { getAllOrders } from '../../redux/selectors/ws'
 import UIActions from '../../redux/actions/ui'
 
 import AtomicOrdersTablePanel from './AtomicOrdersTablePanel'
 
-const mapStateToProps = (state = {}, ownProps = {}) => {
-  const { layoutID, layoutI: id } = ownProps
-
+const mapStateToProps = (state = {}) => {
   return {
-    savedState: getComponentState(state, layoutID, 'atomic-orders', id),
     activeMarket: getActiveMarket(state),
     atomicOrders: getAllOrders(state),
   }
 }
 
 const mapDispatchToProps = dispatch => ({
-  saveState: (layoutID, componentID, state) => {
-    dispatch(UIActions.saveComponentState({
-      state,
-      layoutID,
-      componentID,
-    }))
-  },
   setFilteredValueWithKey: (key, value) => {
     dispatch(UIActions.setFilteredValueWithKey(key, value))
   },
--- src/components/AtomicOrdersTablePanel/AtomicOrdersTablePanel.js
+++ src/components/AtomicOrdersTablePanel/AtomicOrdersTablePanel.js
@@ -1,76 +1,39 @@
-import React from 'react'
+import React, { memo, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import _isEqual from 'lodash/isEqual'
 import AtomicOrdersTable from '../AtomicOrdersTable'
 import Panel from '../../ui/Panel'
 
-class AtomicOrdersTablePanel extends React.Component {
-  state = {
-    filteredAtomicOrders: [],
-  }
+const AtomicOrdersTablePanel = ({
+  dark, onRemove, setFilteredValueWithKey, atomicOrders,
+}) => {
+  useEffect(() => {
+    setFilteredValueWithKey('filteredAtomicOrders', atomicOrders)
+  }, [atomicOrders])
 
-  componentDidMount() {
-    this.getFilteredAtomicOrders()
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    return (!_isEqual(nextProps, this.props) || !_isEqual(nextState, this.state))
-  }
-
-  componentDidUpdate() {}
-  getSnapshotBeforeUpdate() {
-    const atomicOrders = this.getFilteredAtomicOrders()
-    this.setState({ atomicOrders })
-    return null
-  }
-
-  getFilteredAtomicOrders() {
-    const {
-      activeMarket, setFilteredValueWithKey, atomicOrders,
-    } = this.props
-    const { marketFilterActive } = this.state
-    const filteredAtomicOrders = marketFilterActive
-      ? atomicOrders.filter(o => o.symbol === activeMarket.wsID)
-      : atomicOrders
-
-    this.setState({ filteredAtomicOrders })
-    setFilteredValueWithKey('filteredAtomicOrders', filteredAtomicOrders)
-    return filteredAtomicOrders
-  }
-
-  render() {
-    const { onRemove, dark } = this.props
-    const { filteredAtomicOrders } = this.state
-    return (
-      <Panel
-        label='ATOMIC ORDERS'
-        onRemove={onRemove}
-        dark={dark}
-        darkHeader={dark}
-      >
-        <AtomicOrdersTable filteredAtomicOrders={filteredAtomicOrders} />
-      </Panel>
-    )
-  }
+  return (
+    <Panel
+      label='ATOMIC ORDERS'
+      onRemove={onRemove}
+      dark={dark}
+      darkHeader={dark}
+      ddf={3}
+    >
+      <AtomicOrdersTable filteredAtomicOrders={atomicOrders} />
+    </Panel>
+  )
 }
 
 AtomicOrdersTablePanel.propTypes = {
   setFilteredValueWithKey: PropTypes.func.isRequired,
   atomicOrders: PropTypes.arrayOf(PropTypes.object),
-  activeMarket: PropTypes.shape({
-    wsID: PropTypes.string,
-  }),
   onRemove: PropTypes.func,
   dark: PropTypes.bool,
 }
 
 AtomicOrdersTablePanel.defaultProps = {
   atomicOrders: [],
-  activeMarket: {
-    wsID: '',
-  },
   onRemove: () => {},
   dark: true,
 }
 
-export default AtomicOrdersTablePanel
+export default memo(AtomicOrdersTablePanel)
