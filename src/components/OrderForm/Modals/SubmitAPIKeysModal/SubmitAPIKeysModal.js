1dda73f5364d166fc1ffc2a03a2e44615a4cbbb2
Blaumaus <durexp@protonmail.ch>
2021-05-31 23:27:55 +0300

    Refactored SubmitAPIKeysModal into a clear func component

--- src/components/OrderForm/Modals/SubmitAPIKeysModal/SubmitAPIKeysModal.js
+++ src/components/OrderForm/Modals/SubmitAPIKeysModal/SubmitAPIKeysModal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo, useState } from 'react'
 import PropTypes from 'prop-types'
 import _isEmpty from 'lodash/isEmpty'
 
@@ -6,100 +6,49 @@ import Input from '../../../../ui/Input'
 import Button from '../../../../ui/Button'
 import OrderFormModal from '../../OrderFormModal'
 
-class SubmitAPIKeysModal extends React.Component {
-  state = {
-    apiKey: '',
-    apiSecret: '',
-    error: '',
-  }
+const SubmitAPIKeysModal = ({
+  onClose, onSubmit, apiClientConnecting, isPaperTrading, isModal,
+}) => {
+  const [apiKey, setApiKey] = useState('')
+  const [apiSecret, setApiSecret] = useState('')
+  const [error, setError] = useState('')
 
-  constructor(props) {
-    super(props)
-
-    this.onSubmit = this.onSubmit.bind(this)
-    this.onAPIKeyChange = this.onAPIKeyChange.bind(this)
-    this.onAPISecretChange = this.onAPISecretChange.bind(this)
-  }
-
-  onAPIKeyChange(apiKey) {
-    this.setState(() => ({ apiKey }))
-  }
-
-  onAPISecretChange(apiSecret) {
-    this.setState(() => ({ apiSecret }))
-  }
-
-  onSubmit() {
-    const { onSubmit, onClose } = this.props
-    const { apiKey, apiSecret } = this.state
-
+  const submitHandler = () => {
     if (_isEmpty(apiKey)) {
-      this.setState(() => ({ error: 'API Key Required' }))
-    } if (_isEmpty(apiSecret)) {
-      this.setState(() => ({ error: 'API Secret Required' }))
+      setError('API Key Required')
+    } else if (_isEmpty(apiSecret)) {
+      setError('API Secret Required')
     } else {
       onSubmit({ apiKey, apiSecret })
       onClose()
     }
   }
 
-  render() {
-    const {
-      onClose, apiClientConnecting, isPaperTrading, isModal,
-    } = this.props
-    const { apiKey, apiSecret, error } = this.state
-
-    return (
-      <OrderFormModal
-        title={`SUBMIT ${isPaperTrading ? 'PAPER TRADING' : ''} API KEYS`}
-        icon='icon-api'
-        isModal={isModal}
-        apiClientConnecting={apiClientConnecting}
-        form={[
-          <div key='form' className='row'>
-            <Input
-              type='text'
-              placeholder='API KEY'
-              key='apiKey'
-              value={apiKey}
-              onChange={this.onAPIKeyChange}
-            />
-
-            <Input
-              type='password'
-              placeholder='API SECRET'
-              key='apiSecret'
-              value={apiSecret}
-              onChange={this.onAPISecretChange}
-            />
-          </div>,
-
-          error && (
-            <div key='error' className='row'>
-              <p className='error'>{error}</p>
-            </div>
-          ),
-        ]}
-        buttons={(
-          <div className='row'>
-            <Button
-              onClick={this.onSubmit}
-              label='Submit'
-              key='submit'
-              green
-            />
-
-            <Button
-              onClick={onClose}
-              label='Cancel'
-              key='cancel'
-              red
-            />
+  return (
+    <OrderFormModal
+      title={`SUBMIT ${isPaperTrading ? 'PAPER TRADING' : ''} API KEYS`}
+      icon='icon-api'
+      isModal={isModal}
+      apiClientConnecting={apiClientConnecting}
+      form={[
+        <div key='form' className='row'>
+          <Input type='text' placeholder='API KEY' value={apiKey} onChange={setApiKey} />
+          <Input type='password' placeholder='API SECRET' value={apiSecret} onChange={setApiSecret} />
+        </div>,
+        error && (
+          <div key='error' className='row'>
+            <p className='error'>{error}</p>
           </div>
-        )}
-      />
-    )
-  }
+        ),
+      ]}
+      buttons={(
+        <div className='row'>
+          <Button onClick={submitHandler} label='Submit' green />
+          <Button onClick={onClose} label='Cancel' red />
+        </div>
+      )}
+    />
+  )
 }
 
 SubmitAPIKeysModal.propTypes = {
@@ -115,4 +64,4 @@ SubmitAPIKeysModal.defaultProps = {
   apiClientConnecting: false,
 }
 
-export default SubmitAPIKeysModal
+export default memo(SubmitAPIKeysModal)
