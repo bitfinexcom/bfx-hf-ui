c0361bed7761908c1f01d7e665846b52de54f639
dmytroshch <81973123+dmytroshch@users.noreply.github.com>
2021-06-02 14:38:12 +0300

    Changed fiind method from Array.proptotype.find to lodash/find
    Co-authored-by: Andrii R. <margareq@protonmail.ch>

--- src/redux/selectors/meta/get_market_by_symbol.js
+++ src/redux/selectors/meta/get_market_by_symbol.js
@@ -1,8 +1,9 @@
+import _find from 'lodash/find'
 import { REDUCER_PATHS } from '../../config'
 
 const path = REDUCER_PATHS.META
 
 export default (state, symbol) => {
   const { markets } = state[path]
-  return markets.find(market => market.wsID === symbol)
+  return _find(markets, market => market.wsID === symbol)
 }
