67b3cb8840cd39b386b274187d6dd084fd040532
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-06-16 20:50:13 +0300

    PLNumber: adaptived for TickerBar

--- src/ui/PLNumber/index.js
+++ src/ui/PLNumber/index.js
@@ -1,18 +1,50 @@
-import React from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import _isFunction from 'lodash/isFunction'
+import PropTypes from 'prop-types'
 
-import { propTypes, defaultProps } from './PLNumber.props'
-
-export default class PLNumber extends React.PureComponent {
-  static propTypes = propTypes
-  static defaultProps = defaultProps
-
-  render() {
-    const { value, prepareFunc } = this.props
-    const text = _isFunction(prepareFunc) ? prepareFunc(value) : value
-
-    return value < 0
-      ? <span className='hfui-red'>{text}</span>
-      : <span className='hfui-green'>{text}</span>
-  }
+const textStyle = {
+  verticalAlign: 'text-top',
 }
+
+const PLNumber = ({
+  value, prepareFunc, ccy, isGreen,
+}) => {
+  const text = _isFunction(prepareFunc) ? prepareFunc(value) : value
+  const [prevValue, setPrevValue] = useState(0)
+  const [currentValue, setCurrentValue] = useState(prevValue)
+
+  useEffect(() => {
+    setPrevValue(currentValue)
+    setCurrentValue(value)
+  }, [value])
+
+  return (
+    <span className={isGreen ? 'hfui-green' : 'hfui-red'}>
+      <span>
+        {currentValue > prevValue ? <span>&#9650;</span> : <span>&#9660;</span>}
+        &nbsp;
+        <span style={textStyle}>
+          {text}
+          &nbsp;
+          {ccy}
+        </span>
+
+      </span>
+    </span>
+  )
+}
+
+PLNumber.propTypes = {
+  value: PropTypes.number.isRequired,
+  ccy: PropTypes.string,
+  prepareFunc: PropTypes.func,
+  isGreen: PropTypes.bool,
+}
+
+PLNumber.defaultProps = {
+  prepareFunc: null,
+  ccy: '',
+  isGreen: false,
+}
+
+export default memo(PLNumber)
