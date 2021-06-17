5048fca256b2e32540612e3b2733e23f57ca583a
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2021-06-17 22:34:05 +0530

    optimise get_component_state

--- src/redux/selectors/ui/get_component_state.js
+++ src/redux/selectors/ui/get_component_state.js
@@ -1,13 +1,33 @@
 import _get from 'lodash/get'
-import defaultStateForComponentType from './default_state_for_component_type'
+import { createSelector } from 'reselect'
+import getDefaultStateForComponentType from './default_state_for_component_type'
 import { REDUCER_PATHS } from '../../config'
 
 const path = REDUCER_PATHS.UI
 
-export default (state, layoutID, componentType, id) => {
-  return _get(
-    state,
-    `${path}.layoutComponentState.${layoutID}.${id}`,
-    defaultStateForComponentType(state, componentType),
-  )
-}
+const EMPTY_OBJ = {}
+
+const getState = (state) => _get(state, [path, 'layoutComponentState'], EMPTY_OBJ)
+
+const getLayoutState = createSelector(
+  [
+    getState,
+    (_, layoutID) => layoutID,
+  ],
+  (state, layoutID) => _get(state, layoutID, EMPTY_OBJ),
+)
+
+const getComponentState = createSelector(
+  [
+    (state, layoutID) => getLayoutState(state, layoutID),
+    (state, __, componentType) => getDefaultStateForComponentType(state, componentType),
+    (_, __, ___, componentID) => componentID,
+  ],
+  (layoutState, defComponentState, componentID) => _get(
+    layoutState,
+    componentID,
+    defComponentState,
+  ),
+)
+
+export default getComponentState
