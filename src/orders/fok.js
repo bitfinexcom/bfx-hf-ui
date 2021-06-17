fbede529166f16a719a238d6b7195516a4185a64
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-06-17 08:51:52 +0300

    Added TickerBar for all atomic orders

--- src/orders/fok.js
+++ src/orders/fok.js
@@ -45,6 +45,14 @@ export default () => ({
     visible: {
       _context: { eq: 'f' },
     },
+
+  }, {
+    title: '',
+    name: 'ticker',
+    fullWidth: true,
+    rows: [
+      ['ticker'],
+    ],
   }],
 
   fields: {
@@ -63,6 +71,9 @@ export default () => ({
     amount: {
       component: 'input.amount',
       label: 'Amount $BASE',
+    },
+    ticker: {
+      component: 'ui.ticker',
     },
 
     lev: {
