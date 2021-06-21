7fa044c625412add9f5e2c54f5c5c7f4ec651e7b
Blaumaus <durexp@protonmail.ch>
2021-06-21 14:54:38 +0300

    Using dynamic col width for trading stage tables

--- src/components/AlgoOrdersTable/AlgoOrdersTable.columns.js
+++ src/components/AlgoOrdersTable/AlgoOrdersTable.columns.js
@@ -4,11 +4,13 @@ export default (authToken, cancelOrder, gaCancelOrder) => [{
   label: 'Name',
   dataKey: 'name',
   width: 90,
+  flexGrow: 0.7,
   cellRenderer: ({ rowData = {} }) => rowData.name,
 }, {
   label: 'Context',
   dataKey: 'args._margin',
   width: 65,
+  flexGrow: 0.65,
   cellRenderer: ({ rowData = {} }) => {
     return rowData.args?._margin ? 'Margin' : 'Exchange'
   },
@@ -16,21 +18,25 @@ export default (authToken, cancelOrder, gaCancelOrder) => [{
   label: 'Created',
   dataKey: 'gid',
   width: 155,
+  flexGrow: 1.55,
   cellRenderer: ({ rowData = {} }) => new Date(+rowData.gid).toLocaleString(),
 }, {
   label: 'Symbol',
   dataKey: 'args.symbol',
   width: 140,
+  flexGrow: 1.4,
   cellRenderer: ({ rowData = {} }) => rowData.args?.symbol,
 }, {
   label: 'Label',
   dataKey: 'label',
   width: 545,
+  flexGrow: 5.4,
   cellRenderer: ({ rowData = {} }) => rowData.label,
 }, {
   label: 'Actions',
   dataKey: 'cid',
   width: 40,
+  flexGrow: 0.4,
   cellRenderer: ({ rowData = {} }) => ( // eslint-disable-line
     <div className='icons-cell'>
       <i
--- src/components/BalancesTable/BalancesTable.columns.js
+++ src/components/BalancesTable/BalancesTable.columns.js
@@ -6,20 +6,24 @@ export default () => [{
   label: 'Context',
   dataKey: 'context',
   width: 120,
+  flexGrow: 1,
   cellRenderer: ({ rowData = {} }) => _capitalize(rowData.context),
 }, {
   label: 'Currency',
   dataKey: 'currency',
   width: 100,
+  flexGrow: 1,
   cellRenderer: ({ rowData = {} }) => rowData.currency,
 }, {
   label: 'Balance',
   dataKey: 'balance',
   width: 120,
+  flexGrow: 1,
   cellRenderer: ({ rowData = {} }) => processBalance(prepareAmount(rowData.balance)),
 }, {
   label: 'Available',
   dataKey: 'available',
   width: 120,
+  flexGrow: 1,
   cellRenderer: ({ rowData = {} }) => processBalance(prepareAmount(rowData.available)),
 }]
