74fba0fb0283c682414c4c77930ec864ad86b3eb
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-06-25 14:02:19 +0300

    OrderForm: fill price form on ticker click

--- src/components/OrderForm/FieldComponents/ui.ticker.js
+++ src/components/OrderForm/FieldComponents/ui.ticker.js
@@ -1,28 +1,52 @@
 import React, { memo } from 'react'
+import PropTypes from 'prop-types'
 import { useSelector } from 'react-redux'
 import { getTicker } from '../../../redux/selectors/meta'
 import { getActiveMarket } from '../../../redux/selectors/ui'
 import PLNumber from '../../../ui/PLNumber'
 import { localiseNumber } from '../../../util/ui'
 
-const TickerBar = () => {
+const FIELD_TO_CHANGE = 'price'
+
+const TickerBar = ({ onFieldChange }) => {
   const activeMarket = useSelector(getActiveMarket)
   const activeMarketTicker = useSelector((state) => getTicker(state, activeMarket))
 
   const { bid, ask } = activeMarketTicker
   const { quote } = activeMarket
+
+  const setInputPrice = (value) => onFieldChange(FIELD_TO_CHANGE, value)
   return (
     <div className='hfui-orderform__ticker-container'>
-      <div className='ticker-section'>
+      <div
+        className='ticker-section'
+        onClick={() => setInputPrice(bid)}
+        role='button'
+        tabIndex='0'
+      >
         <span className='ticker-name'>BID:</span>
-        <PLNumber value={bid} prepareFunc={localiseNumber} ccy={quote} isGreen />
+        <PLNumber
+          value={bid}
+          prepareFunc={localiseNumber}
+          ccy={quote}
+          isGreen
+        />
       </div>
-      <div className='ticker-section'>
+      <div
+        className='ticker-section'
+        onClick={() => setInputPrice(ask)}
+        role='button'
+        tabIndex='0'
+      >
         <span className='ticker-name'>ASK:</span>
         <PLNumber value={ask} prepareFunc={localiseNumber} ccy={quote} />
       </div>
     </div>
   )
+}
+
+TickerBar.propTypes = {
+  onFieldChange: PropTypes.func.isRequired,
 }
 
 export default memo(TickerBar)
