870ecf7111bd50ccac620d13cea49f607acd266a
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-07-08 14:09:21 +0300

    Showing right ccy pairs in AlgoOrdersTable

--- src/redux/selectors/ws/get_algo_orders.js
+++ src/redux/selectors/ws/get_algo_orders.js
@@ -1,8 +1,22 @@
 import _get from 'lodash/get'
+import _map from 'lodash/map'
+import _find from 'lodash/find'
+import { createSelector } from 'reselect'
 import { REDUCER_PATHS } from '../../config'
+import { getMarkets } from '../meta'
 
 const path = REDUCER_PATHS.WS
 
-export default (state) => {
+const algoOrders = (state) => {
   return _get(state, `${path}.algoOrders`, [])
 }
+const allMarkets = state => getMarkets(state)
+
+const algoOrdersWithReplacedPairs = createSelector([allMarkets, algoOrders], (markets, orders) => {
+  return _map(orders, (order) => {
+    const currentMarket = _find(markets, (market) => market.wsID === order.args.symbol)
+    return { ...order, args: { ...order.args, symbol: currentMarket.uiID } }
+  }, [])
+})
+
+export default algoOrdersWithReplacedPairs
