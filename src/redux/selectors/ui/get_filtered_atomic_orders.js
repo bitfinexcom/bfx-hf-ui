7958322ec0f6b98961059f5797410627bdba1022
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-07-08 14:02:39 +0300

    Replced logic for atomic orders from ui to ws

--- src/redux/selectors/ui/get_filtered_atomic_orders.js
+++ src/redux/selectors/ui/get_filtered_atomic_orders.js
@@ -1,9 +1,5 @@
 import _get from 'lodash/get'
-import _map from 'lodash/map'
-import _find from 'lodash/find'
-import { createSelector } from 'reselect'
 import { REDUCER_PATHS } from '../../config'
-import { getMarkets } from '../meta'
 
 const path = REDUCER_PATHS.UI
 
@@ -11,13 +7,5 @@ const filteredAtomicOrders = (state) => {
   const filteredAtomics = _get(state, `${path}.filteredAtomicOrders`, [])
   return filteredAtomics
 }
-const allMarkets = state => getMarkets(state)
 
-const atomicOrdersWithReplacedPairs = createSelector([allMarkets, filteredAtomicOrders], (markets, orders) => {
-  return _map(orders, (order) => {
-    const currentMarket = _find(markets, (market) => market.wsID === order.symbol)
-    return { ...order, symbol: currentMarket.uiID }
-  }, [])
-})
-
-export default atomicOrdersWithReplacedPairs
+export default filteredAtomicOrders
