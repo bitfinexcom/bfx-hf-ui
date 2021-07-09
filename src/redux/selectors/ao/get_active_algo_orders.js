4c3d3febd0d30c3405baca791a61d8efdb407307
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-07-09 10:48:56 +0300

    Showing active algo orders with right ccy pairs

--- src/redux/selectors/ao/get_active_algo_orders.js
+++ src/redux/selectors/ao/get_active_algo_orders.js
@@ -1,8 +1,23 @@
 import _get from 'lodash/get'
+import _map from 'lodash/map'
+import _find from 'lodash/find'
+import { createSelector } from 'reselect'
 import { REDUCER_PATHS } from '../../config'
+import { getMarkets } from '../meta'
 
 const path = REDUCER_PATHS.AOS
 
-export default (state) => {
+const activeAlgoOrders = (state) => {
   return _get(state, `${path}.activeAlgoOrders`, [])
 }
+
+const allMarkets = state => getMarkets(state)
+
+const activeAlgoOrdersWithReplacedPairs = createSelector([allMarkets, activeAlgoOrders], (markets, orders) => {
+  return _map(orders, (order) => {
+    const currentMarket = _find(markets, (market) => market.wsID === order.args.symbol)
+    return { ...order, args: { ...order.args, symbol: currentMarket.uiID } }
+  }, [])
+})
+
+export default activeAlgoOrdersWithReplacedPairs
