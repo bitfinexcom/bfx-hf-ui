3a1a5215affd5433c445e196d965be8e383e8c60
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-07-09 10:49:16 +0300

    Showing positions with right ccy pairs

--- src/redux/selectors/ws/get_all_positions.js
+++ src/redux/selectors/ws/get_all_positions.js
@@ -1,10 +1,25 @@
 import _get from 'lodash/get'
+import _map from 'lodash/map'
+import _find from 'lodash/find'
+import { createSelector } from 'reselect'
 import { REDUCER_PATHS } from '../../config'
+import { getMarkets } from '../meta'
 
 const path = REDUCER_PATHS.WS
 
 const EMPTY_ARR = []
 
-export default (state) => {
+const allPositions = (state) => {
   return _get(state, `${path}.positions`, EMPTY_ARR)
 }
+
+const allMarkets = state => getMarkets(state)
+
+const positionWithReplacedPairs = createSelector([allMarkets, allPositions], (markets, positions) => {
+  return _map(positions, (position) => {
+    const currentMarket = _find(markets, (market) => market.wsID === position.symbol)
+    return { ...position, symbol: currentMarket.uiID }
+  }, [])
+})
+
+export default positionWithReplacedPairs
