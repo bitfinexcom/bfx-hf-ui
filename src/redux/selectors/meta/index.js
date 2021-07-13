9c868b013764664f1c145320a2eaf39926185797
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-07-13 10:40:53 +0300

    Markets: selector of getting markets object

--- src/redux/selectors/meta/index.js
+++ src/redux/selectors/meta/index.js
@@ -1,4 +1,5 @@
 import getMarkets from './get_markets'
+import getMarketsObject from './get_markets_object'
 import getMarketBySymbol from './get_market_by_symbol'
 import getTicker from './get_ticker'
 import getMarketTickersKeys from './get_market_tickers_keys'
@@ -6,6 +7,7 @@ import getTickersArray from './get_tickers_array'
 
 export {
   getMarkets,
+  getMarketsObject,
   getMarketBySymbol,
   getTicker,
   getTickersArray,
