fa5c0423f6029a41517242a8c56383a634d3c7e0
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-07-13 10:56:34 +0300

    Markets: usuing market's objects instead of array

--- src/redux/selectors/ao/get_active_algo_orders.js
+++ src/redux/selectors/ao/get_active_algo_orders.js
@@ -1,9 +1,8 @@
 import _get from 'lodash/get'
 import _map from 'lodash/map'
-import _find from 'lodash/find'
 import { createSelector } from 'reselect'
 import { REDUCER_PATHS } from '../../config'
-import { getMarkets } from '../meta'
+import { getMarketsObject } from '../meta'
 
 const path = REDUCER_PATHS.AOS
 
@@ -11,11 +10,12 @@ const activeAlgoOrders = (state) => {
   return _get(state, `${path}.activeAlgoOrders`, [])
 }
 
-const allMarkets = state => getMarkets(state)
+const allMarkets = state => getMarketsObject(state)
 
 const activeAlgoOrdersWithReplacedPairs = createSelector([allMarkets, activeAlgoOrders], (markets, orders) => {
   return _map(orders, (order) => {
-    const currentMarket = _find(markets, (market) => market.wsID === order.args.symbol)
+    const { symbol } = order.args
+    const currentMarket = markets[symbol]
     return { ...order, args: { ...order.args, symbol: currentMarket.uiID } }
   }, [])
 })
--- src/redux/selectors/ws/get_all_orders.js
+++ src/redux/selectors/ws/get_all_orders.js
@@ -1,20 +1,20 @@
 import _get from 'lodash/get'
 import _map from 'lodash/map'
-import _find from 'lodash/find'
 import { createSelector } from 'reselect'
 import { REDUCER_PATHS } from '../../config'
-import { getMarkets } from '../meta'
+import { getMarketsObject } from '../meta'
 
 const path = REDUCER_PATHS.WS
 
 const atomicOrders = (state) => {
   return _get(state, `${path}.orders`, [])
 }
-const allMarkets = state => getMarkets(state)
+const allMarkets = state => getMarketsObject(state)
 
 const atomicOrdersWithReplacedPairs = createSelector([allMarkets, atomicOrders], (markets, orders) => {
   return _map(orders, (order) => {
-    const currentMarket = _find(markets, (market) => market.wsID === order.symbol)
+    const { symbol } = order
+    const currentMarket = markets[symbol]
     return { ...order, symbol: currentMarket.uiID }
   }, [])
 })
