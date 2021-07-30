a3c4908b13b87e83cb37915d9ed0615e0925a1f1
Dmytro Shcherbonos <dmytro.sh@code-care.pro>
2021-07-30 08:43:13 +0300

    Removed 'Edit' in top menu

--- public/lib/app_menu_template.js
+++ public/lib/app_menu_template.js
@@ -16,17 +16,6 @@ module.exports = app => ([{
     },
   }],
 }, {
-  label: 'Edit',
-  submenu: [
-    { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
-    { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
-    { type: 'separator' },
-    { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
-    { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
-    { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
-    { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' },
-  ],
-}, {
   label: 'Diagnostics',
   submenu: [{
     label: 'Open Logs Folder',
