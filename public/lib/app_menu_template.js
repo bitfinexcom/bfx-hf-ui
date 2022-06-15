eed10ad8bede36e00afea018b751e45a57303a97
Tarcisio Ferraz <tarcisio.ferraz@bitfinex.com>
2022-06-15 15:20:45 -0300

    fix app menu

--- public/lib/app_menu_template.js
+++ public/lib/app_menu_template.js
@@ -9,20 +9,18 @@ const {
 } = require('../constants')
 
 const RC_KEYWORD = '-rc'
-const appVersion = process.env.npm_package_version
-const isRCMode = appVersion.includes(RC_KEYWORD)
 
 const RC_MENUS = [
   {
     label: 'Toggle Developer Tools',
-    accelerator: (function() {
+    accelerator: (function () {
       const platform = os.platform()
-      if (platform == 'darwin') {
+      if (platform === 'darwin') {
         return 'Alt+Command+I';
       }
       return 'Ctrl+Shift+I';
     })(),
-    click: function(item, focusedWindow) {
+    click: function (item, focusedWindow) {
       if (focusedWindow) {
         focusedWindow.toggleDevTools();
       }
@@ -43,51 +41,57 @@ const RC_MENUS = [
   }
 ]
 
-module.exports = (app, mainWindow) => ([
-{
-  label: 'Application',
-  submenu: [{
-    label: 'Quit',
-    accelerator: 'CmdOrCtrl+Q',
-    click: () => {
-      app.quit()
-    },
-  }],
-}, {
-  label: 'Edit',
-  submenu: [
-    { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
-    { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
-    { type: 'separator' },
-    { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
-    { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
-    { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
-    { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' },
-  ],
-}, {
-  label: 'Diagnostics',
-  submenu: [{
-      label: 'Open Logs Folder',
-      click: () => {
-        open(LOG_PATH).catch((e) => {
-          console.error(`failed to open logs folder: ${e.message}`)
-        })
-      },
+module.exports = (app) => {
+  const appVersion = process.env.npm_package_version
+  const isRCMode = appVersion && appVersion.includes(RC_KEYWORD)
+
+  return [
+    {
+      label: 'Application',
+      submenu: [{
+        label: 'Quit',
+        accelerator: 'CmdOrCtrl+Q',
+        click: () => {
+          app.quit()
+        },
+      }],
     }, {
-      label: 'Open Data Server Log',
-      click: () => {
-        open(LOG_PATH_DS_BITFINEX).catch((e) => {
-          console.error(`failed to open data server log file: ${e.message}`)
-        })
-      },
+      label: 'Edit',
+      submenu: [
+        {label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:'},
+        {label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:'},
+        {type: 'separator'},
+        {label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:'},
+        {label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:'},
+        {label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:'},
+        {label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:'},
+      ],
     }, {
-      label: 'Open API Server Log',
-      click: () => {
-        open(LOG_PATH_API_SERVER).catch((e) => {
-          console.error(`failed to open api server log file: ${e.message}`)
-        })
+      label: 'Diagnostics',
+      submenu: [{
+        label: 'Open Logs Folder',
+        click: () => {
+          open(LOG_PATH).catch((e) => {
+            console.error(`failed to open logs folder: ${e.message}`)
+          })
+        },
+      }, {
+        label: 'Open Data Server Log',
+        click: () => {
+          open(LOG_PATH_DS_BITFINEX).catch((e) => {
+            console.error(`failed to open data server log file: ${e.message}`)
+          })
+        },
+      }, {
+        label: 'Open API Server Log',
+        click: () => {
+          open(LOG_PATH_API_SERVER).catch((e) => {
+            console.error(`failed to open api server log file: ${e.message}`)
+          })
+        },
       },
-    },
-    ...(isRCMode ? RC_MENUS : []),
+        ...(isRCMode ? RC_MENUS : []),
+      ]
+    }
   ]
-}])
+}
