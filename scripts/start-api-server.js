24e42d591ab978333b38ad4857ade1e8f7485ee7
Tarcisio Ferraz <tarcisio.ferraz@bitfinex.com>
2022-08-11 19:41:30 -0300

    add metrics info

--- scripts/start-api-server.js
+++ scripts/start-api-server.js
@@ -6,6 +6,7 @@ require('bfx-hf-util/lib/catch_uncaught_errors')
 
 const startHFServer = require('bfx-hf-server')
 const os = require('os')
+const { version } = require('../package.json')
 
 const dir = `${os.homedir()}/.bitfinexhoney`
 
@@ -18,6 +19,11 @@ startHFServer({
   bfxRestURL: 'https://api.bitfinex.com/',
   bfxHostedWsUrl: process.env.HOSTED_WS_URL,
   strategyExecutionPath: `${dir}/strategy-executions.json`,
+
+  bfxMetricsWsUrl: 'ws://localhost:9090',
+  os: process.platform,
+  releaseVersion: version,
+  isRC: version.includes('rc'),
 
   // Data servers are started by individual scripts
   // hfBitfinexDBPath: `${__dirname}/db/hf-bitfinex.json`,
